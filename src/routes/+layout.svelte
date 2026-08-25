d3a319493a7d2f206fae3ed8555887dc0b48ca72
Monty Pages <matmontypages@gmail.com>
2026-08-24 20:37:30 -0700

    installed pwa...again?

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,20 +1,18 @@
 <script>
-	import favicon from '$lib/assets/favicon.svg';
-	import "../lib/style/app.css"
+	import favicon from '$lib/assets/favicon-32.png';
+	import '../lib/style/app.css';
 
 	let { children } = $props();
 </script>
 
 <svelte:head>
 	<link rel="icon" href={favicon} />
+	<link rel="apple-touch-icon" href="/apple-touch-icon.png" />
 </svelte:head>
 
 <h1>Riley's Sports App</h1>
 
-
-
 {@render children()}
-
 
 <style>
 	h1 {
